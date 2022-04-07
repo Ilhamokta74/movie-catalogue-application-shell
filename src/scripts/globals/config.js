235ea7ffed42b430f25ee43f1cbfac80b4e3db04
ilhamokta74 <ilhamoktavian74@gmail.com>
2022-04-07 21:17:53 +0700

    start

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'b01cb2f2e32f08fd8a83dcd941549867',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
